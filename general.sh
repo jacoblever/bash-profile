9d1c7c4f4cf78d6a4f37616eebcad9973c6874f2
Jacob Lever <jacob.lever@deliveroo.co.uk>
2019-04-29 15:44:04 +0100

    Add command to cd to this repo

--- general.sh
+++ general.sh
@@ -24,6 +24,7 @@ open-url() {
 # This file
 alias edit-profile="op ~/.bash_profile"
 alias edit-profile-general="op ~/bash-profile/general.sh"
+alias cd-bash-profile="open -a Terminal ~/bash-profile/"
 alias reload-profile="source ~/.bash_profile"
 alias pull-bash-profile="cd ~/bash-profile/ && git pull && reload-profile"
 
